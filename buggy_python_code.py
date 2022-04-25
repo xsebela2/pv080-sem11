228ccfe42fa4eb143f010c2370601cde28967d8f
xsebela2 <514509@mail.muni.cz>
2022-04-25 17:27:47 +0200

    I have no idea what to do here anyway

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,19 +1,25 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
 import base64
+import pickle
+import subprocess
 import flask
 
 # Input injection
+from flask import app
+
+
 def transcode_file(request, filename):
-    command = f'ffmpeg -i "{filename}" output_file.mpg'
+    sanitized = filename.replace("\"", "\\\"")
+    command = f'ffmpeg -i {sanitized} output_file.mpg'
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
 def foo(request, user):
-    assert user.is_admin, 'user does not have access'
+    if not user.is_admin:
+        raise PermissionError("user does not have access")
+    request(user)
     # secure code...
 
 
@@ -23,7 +29,7 @@ class RunBinSh:
         return (subprocess.Popen, (('/bin/sh',),))
 
 def import_urlib_version(version):
-    exec("import urllib%s as urllib" % version)
+    exec(f"import urllib{version} as urllib")
 
 @app.route('/')
 def index():
